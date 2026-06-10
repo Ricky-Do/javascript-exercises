080e15b9fdb5180036a736fa01ec049e509aeeca
Ricky Do <rickydo825@gmail.com>
2026-06-10 18:56:21 -0500

    Completed 06 excersice

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,6 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(str, nums) {
+    let string = "";
+    for(let i  = 0; i<nums; i++){
+        string = string + str;
+    }
+    return string;
 };
+
+console.log(repeatString("hey", 3));
 
 // Do not edit below this line
 module.exports = repeatString;

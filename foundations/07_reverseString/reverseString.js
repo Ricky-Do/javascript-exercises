b7ad87cd0589382736bb7381328014008aab6495
Ricky Do <rickydo825@gmail.com>
2026-06-10 19:45:47 -0500

    Completed 07 excersice

--- foundations/07_reverseString/reverseString.js
+++ foundations/07_reverseString/reverseString.js
@@ -1,6 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    let reversedString = str.split("").reverse().join("");
+    return reversedString;
 };
+
+console.log(reverseString("Hello"));
 
 // Do not edit below this line
 module.exports = reverseString;
